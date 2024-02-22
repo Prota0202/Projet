f4d8986a14f68364be67fac0ef6492ddf51da1bb
Mister-I.Z <20057@ecam.be>
2024-02-22 21:55:00 +0100

    The MainPage of the app

--- App2/Customer_app/MainPage.xaml.cs
+++ App2/Customer_app/MainPage.xaml.cs
@@ -1,24 +1,19 @@
-﻿namespace Customer_app;
+﻿using Customer_app.Views;
+
+namespace Customer_app;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void Orderbuttonclicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		Navigation.PushAsync(new OrderingPage()); 
 	}
+
 }
 
