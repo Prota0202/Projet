b1d364adb40484bdd2830eabe83873816e8cb7e3
SamirBch <20053@ecam.be>
2024-02-26 19:15:00 +0100

    Add Order class

---
+++ App2/Customer_app/Models/Order.cs
@@ -0,0 +1,14 @@
+namespace Customer_app.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Depth { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string PanelColor { get; set; }
+        public string Door { get; set; }
+        public string AngleIronColor { get; set; }
+        public string Comment { get; set; }
+    }
+}
