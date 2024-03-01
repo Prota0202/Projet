d16ba8ad7892eeb7efe6e7f690cd3740fd31f657
Mister-I.Z <20057@ecam.be>
2024-03-01 08:50:31 +0100

    Add button "BasketPage" (2)

--- App2/Customer_app/Views/OrderPage.xaml.cs
+++ App2/Customer_app/Views/OrderPage.xaml.cs
@@ -1,9 +1,15 @@
-namespace MauiApp1;
+namespace Customer_app.Views;
 
 public partial class OrderPage : ContentPage
 {
 	public OrderPage()
 	{
 		InitializeComponent();
+	}
+
+	private void basketclicked(object sender, EventArgs e)
+	{
+
+		Navigation.PushAsync(new BasketPage());
 	}
 }
