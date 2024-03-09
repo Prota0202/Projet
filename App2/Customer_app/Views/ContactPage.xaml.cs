dbee21c2fa8e8172bf08dc96f9d32eadbe991fa3
Timothé Couturier <99717711+TimCout@users.noreply.github.com>
2024-03-09 11:43:08 +0100

    Modif Contact page + AddContact function

--- App2/Customer_app/Views/ContactPage.xaml.cs
+++ App2/Customer_app/Views/ContactPage.xaml.cs
@@ -1,6 +1,10 @@
+using Customer_app.Models;
+using System.Collections.ObjectModel;
 namespace Customer_app.Views;
+
 public partial class ContactPage : ContentPage
 {
+	public ObservableCollection<Customer> customerList {get; set;} = new ObservableCollection<Customer>();
 	public ContactPage()
 	{
 		InitializeComponent();
@@ -9,5 +13,15 @@ public partial class ContactPage : ContentPage
 	private void OrderBackbuttonclicked(object sender, EventArgs e)
 	{
 		Navigation.PopAsync();
+	}
+
+	private void OnAddContactClicked(object sender, EventArgs e)
+	{
+		string customerName = NameContact.Text;
+		string customerEmail = EmailContact.Text;
+		int customerPhone = Convert.ToInt32(PhoneContact.Text);
+		Customer newCustomer = new Customer(customerName, customerEmail, customerPhone);
+		customerList.Add(newCustomer);
+
 	}
 }
