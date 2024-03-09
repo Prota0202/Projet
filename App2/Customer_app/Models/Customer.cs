5a0567aa41bbfc34cd8969c309c63715fb41067c
Timothé Couturier <99717711+TimCout@users.noreply.github.com>
2024-03-09 11:42:47 +0100

    Creation Customer Class

---
+++ App2/Customer_app/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Customer_app.Models;
+
+public class Customer
+{
+	public string CustomerName;
+	public string CustomerEmail;
+	public int CustomerPhone;
+
+	public Customer(string CustomerName, string CustomerEmail, int CustomerPhone)
+	{
+		this.CustomerName = CustomerName;
+		this.CustomerEmail = CustomerEmail;
+		this.CustomerPhone = CustomerPhone;
+	}
+
+	public string DisplayCustomer => $"{CustomerName} - {CustomerEmail} - {CustomerPhone}";
+}
